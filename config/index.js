72a82e530d488374504b8691b5d4863ed1cca9d5
xiii <xiii.at.cn@gmail.com>
2017-11-03 15:35:00 +0800

    add module vue-quill-editor

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:80',
+        target: 'http://192.168.1.97:1080',
         pathRewrite: {
           '^/api': '/api'
         }
