68c0d9b71a63f0289de6f29811b0e624180e227b
Fangzy <1003455913@qq.com>
2017-11-16 18:16:29 +0800

    change m-input-choose.vue

--- src/components/base/m-input-choose.vue
+++ src/components/base/m-input-choose.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        <div class="search">
-            <Input v-model="inputValue" :placeholder="placeholder" readonly disabled >
-                <Button slot="append" @click="handleChooseClick">选择</Button>
-            </Input>
-        </div>
-        <mLayer v-if="showLayer" :value="showLayer" :titleText="placeholder" @on-cancel="handleCancel" @on-ok="handleOk">
-            <component
-                :is="downData.ui_content[0].ui_type"
-                :uid="downData.ui_content[0].ui_id"
-            ></component>
-        </mLayer>
+        <Row>
+            <div class="search">
+                <Input v-model="inputValue" :placeholder="placeholder" readonly disabled >
+                    <Button slot="append" @click="handleChooseClick">选择</Button>
+                </Input>
+            </div>
+            <mLayer v-if="showLayer" :value="showLayer" :titleText="placeholder" @on-cancel="handleCancel" @on-ok="handleOk">
+                <component
+                    :is="downData.ui_content[0].ui_type"
+                    :uid="downData.ui_content[0].ui_id"
+                    :define="downData.ui_content[0].ui_define"
+                ></component>
+            </mLayer>
+        </Row>
+        <Row>
+            <div v-if="hasError" class="gateway-item-error">{{errorMessage}}</div>
+            <div v-else class="occupation gateway-item-error">隐藏</div>
+        </Row>
     </div>
 </template>
 <script>
@@ -20,7 +27,7 @@
     import {getData} from 'utils/actionUtils'
 
 export default {
-    name: 'm-input-percent',
+    name: 'm-input-choose',
     mixins: [mixin],
     data () {
         return {
@@ -48,14 +55,44 @@ export default {
         },
         handleCancel(){
             this.showLayer = false
-
+            this.inputCheck()
         },
         handleOk(){
             this.showLayer = false
-
+            this.getChooseData()
+            this.inputCheck()
         },
         getChooseData(){
             //this.inputValue = _.get(this.$store.state., , '')
+        },
+        valid () {
+            if (!this.readonly) {
+                let hasError = false
+                let value = String(this.objectModel == undefined ? '' : this.objectModel)
+                if (this.required) {
+                    if (value === '') {
+                        hasError = true
+                        this.errorMessage = '请输入必填项'
+                    } else if (value.length < this.minLength) {
+                        hasError = true
+                        this.errorMessage = `输入最小长度应不小于${this.minLength}`
+                    } else if (value.length > this.maxLength) {
+                        hasError = true
+                        this.errorMessage = `输入最大长度应不大于${this.maxLength}`
+                    } else {
+                        let regex = new RegExp(this.pattern)
+                        if (!regex.test(this.objectModel)) {
+                            hasError = true
+                            this.errorMessage = '输入不符合要求, 请重新输入'
+                        }
+                    }
+                }
+                this.$store.dispatch(ELEMENT_VALIDATE_RESULT, {[this.name]: hasError, form: this.form})
+            }
+        },
+        inputCheck () {
+            debugger
+            this.valid()
         }
     }
 }
