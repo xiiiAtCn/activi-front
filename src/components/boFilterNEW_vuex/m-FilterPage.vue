9ffef855bd0eb763201a04cc8d7e0d6278734009
zhao peng <719197352@qq.com>
2017-11-17 15:57:20 +0800

    添加 nexttick

--- src/components/boFilterNEW_vuex/m-FilterPage.vue
+++ src/components/boFilterNEW_vuex/m-FilterPage.vue
@@ -20,6 +20,94 @@
 import _ from 'lodash'
 import { deepCopy } from 'utils/utils'
 
+let navSelectDefine = {
+    isRelated: true,
+    relation: [],
+    dataLink: [
+        {
+            attr: 'selectData',
+            link: {
+                method: 'GET',
+                url: '',
+                pathParams: {},
+                queryParams: {
+                    id: {
+                        value: '',
+                        defaultValue: ''
+                    }
+                },
+                body: {}
+            }
+        }
+    ],
+    id: '',
+} 
+
+let filterDefine = {
+    isRelated: true,
+    relation: [this.navSelectId],
+    dataLink: [
+        {
+            attr: 'bodyData',
+            link: {
+                method: 'GET',
+                url: '',
+                pathParams: {},
+                queryParams: {
+                    id: {
+                        value: this.navSelectId,
+                        defaultValue: ''
+                    }
+                },
+                body: {}
+            }
+        }
+    ],
+    id: '',
+}
+
+let tableDefine = {
+    isRelated: true,
+    relation: [],
+    dataLink: [
+        {
+            attr: 'tableData',
+            link: {
+                method: 'POST',
+                url: '',
+                pathParams: {},
+                queryParams: {},
+                body: {
+                    id: {
+                        value: '',
+                        defaultValue: ''
+                    },
+                    condition: {
+                        value: '',
+                        defaultValue: {}
+                    }
+                }
+            }
+        },
+        {
+            attr: 'tableDefine',
+            link: {
+                method: 'GET',
+                url: '',
+                pathParams: {},
+                queryParams: {
+                    id: {
+                        value: '',
+                        defaultValue: ''
+                    }
+                },
+                body: {}
+            }
+        }
+    ],
+    id: '',
+}
+
 export default{
     props: {
         define: {
@@ -27,119 +115,42 @@ export default{
         },
         form: {
             type: String,
-            default: ''
+            default: 'ceshi'
         }
     },
     data () {
         return {
-            navSelectDefine: {
-                isRelated: true,
-                relation: [],
-                dataLink: [
-                    {
-                        attr: 'selectData',
-                        link: {
-                            method: 'GET',
-                            url: '',
-                            pathParams: {},
-                            queryParams: {
-                                id: {
-                                    value: '',
-                                    defaultValue: ''
-                                }
-                            },
-                            body: {}
-                        }
-                    }
-                ],
-                id: '',
-            },
-            filterDefine: {
-                isRelated: true,
-                relation: [this.navSelectId],
-                dataLink: [
-                    {
-                        attr: 'bodyData',
-                        link: {
-                            method: 'GET',
-                            url: '',
-                            pathParams: {},
-                            queryParams: {
-                                id: {
-                                    value: this.navSelectId,
-                                    defaultValue: ''
-                                }
-                            },
-                            body: {}
-                        }
-                    }
-                ],
-                id: '',
-            },
-            tableDefine: {
-                isRelated: true,
-                relation: [],
-                dataLink: [
-                    {
-                        attr: 'tableData',
-                        link: {
-                            method: 'POST',
-                            url: '',
-                            pathParams: {},
-                            queryParams: {},
-                            body: {
-                                id: {
-                                    value: '',
-                                    defaultValue: ''
-                                },
-                                condition: {
-                                    value: '',
-                                    defaultValue: {}
-                                }
-                            }
-                        }
-                    },
-                    {
-                        attr: 'tableDefine',
-                        link: {
-                            method: 'GET',
-                            url: '',
-                            pathParams: {},
-                            queryParams: {
-                                id: {
-                                    value: '',
-                                    defaultValue: ''
-                                }
-                            },
-                            body: {}
-                        }
-                    }
-                ],
-                id: '',
-            }
+            navSelectDefine: {},
+            filterDefine: {},
+            tableDefine: {}
         }
     },
     mounted () {
-        // 设置id
-        this.$set(this.navSelectDefine, 'id', this.navSelectId)
-        this.$set(this.filterDefine, 'id', this.filterId)
-        this.$set(this.tableDefine, 'id', this.tableId)
+        this.$nextTick(() => {
+            this.navSelectDefine = navSelectDefine
+            this.filterDefine = filterDefine
+            this.tableDefine = tableDefine
+            // 设置id
+            this.$set(this.navSelectDefine, 'id', this.navSelectId)
+            this.$set(this.filterDefine, 'id', this.filterId)
+            this.$set(this.tableDefine, 'id', this.tableId)
 
-        // 设置url
-        this.$set(this.navSelectDefine.dataLink[0].link, 'url', this.navDataUrl)
-        this.$set(this.filterDefine.dataLink[0].link, 'url', this.filterDataUrl)
-        this.$set(this.tableDefine.dataLink[0].link, 'url', this.tableDataUrl)
-        this.$set(this.tableDefine.dataLink[1].link, 'url', this.tableDefineUrl)
+            // 设置url
+            this.$set(this.navSelectDefine.dataLink[0].link, 'url', this.navDataUrl)
+            this.$set(this.filterDefine.dataLink[0].link, 'url', this.filterDataUrl)
+            this.$set(this.tableDefine.dataLink[0].link, 'url', this.tableDataUrl)
+            this.$set(this.tableDefine.dataLink[1].link, 'url', this.tableDefineUrl)
 
-        // 设置关联关系
-        this.$set(this.navSelectDefine, 'relation', [this.navSelectId])
-        this.$set(this.navSelectDefine.dataLink[0].link.queryParams.id, 'value', this.navSelectId)
-        this.$set(this.filterDefine, 'relation', [this.navSelectId])
-        this.$set(this.filterDefine.dataLink[0].link.queryParams.id, 'value', this.navSelectId)
-        this.$set(this.tableDefine, 'relation', [this.navSelectId, this.filterId])
-        this.$set(this.tableDefine.dataLink[0].link.body.id, 'value', this.navSelectId)
-        this.$set(this.tableDefine.dataLink[0].link.body.condition, 'value', this.filterId)
-        this.$set(this.tableDefine.dataLink[1].link.queryParams.id, 'value', this.navSelectId)
+            // 设置关联关系
+            this.$set(this.navSelectDefine, 'relation', [this.navSelectId])
+            this.$set(this.navSelectDefine.dataLink[0].link.queryParams.id, 'value', this.navSelectId)
+            this.$set(this.filterDefine, 'relation', [this.navSelectId])
+            this.$set(this.filterDefine.dataLink[0].link.queryParams.id, 'value', this.navSelectId)
+            this.$set(this.tableDefine, 'relation', [this.navSelectId, this.filterId])
+            this.$set(this.tableDefine.dataLink[0].link.body.id, 'value', this.navSelectId)
+            this.$set(this.tableDefine.dataLink[0].link.body.condition, 'value', this.filterId)
+            this.$set(this.tableDefine.dataLink[1].link.queryParams.id, 'value', this.navSelectId)
+        })
     },
     computed: {
         navDataUrl () {
@@ -163,7 +174,7 @@ export default{
         tableId () {
             return _.get(this.define, 'dataListName', 'table')
         },
-    }
+    },
 }
 </script>
 <style scoped>
