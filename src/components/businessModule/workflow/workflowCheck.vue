0f6d760d8601c3b1d6f2a8c600ed517422da1502
yangdongyan <735418023@qq.com>
2017-11-23 15:35:47 +0800

    2017/11/23 ydy 1. 工作流程

--- src/components/businessModule/workflow/workflowCheck.vue
+++ src/components/businessModule/workflow/workflowCheck.vue
@@ -1,51 +1,57 @@
 <template>
     <div class="CheckWrap">
         <Row>
+            <Col span="23">
+                <h2 class="marBot" style="float: left">{{flowName}}</h2>
+                <Button @click="goBack" style="float: right">返回</Button>
+            </Col>
+        </Row>
+        <Row>
             <Col span="9">
                 <Table :columns="checkColumns" :data="checkData" :border="tableBorder"></Table>
             </Col>
             <Col span="13" push="1">
-                <Row>
-                    <Button type="info" @click="showMsg = false">文字信息</Button>
-                    <Button type="success" @click="showMsg = true">图形信息</Button>
-                </Row>
-                <div class="detailsBox top_16">
-                    <div v-if="!showMsg">
-                        <Row>
-                            <ButtonGroup>
-                                <Button>检查项</Button>
-                                <Button>CHECK-LIST</Button>
-                                <Button>输入单据</Button>
-                                <Button>输出单据</Button>
-                                <Button>状态转换</Button>
-                                <Button>单据转换规则</Button>
-                            </ButtonGroup>
-                        </Row>
-                        <Row class="top_16">
-                            <h3 class="lh_24">{{ flowName }} --> 负责人：{{ leaderName }}</h3>
-                            <h3 class="lh_24">目标：</h3>
-                        </Row>
-                        <Row class="top_16">
-                            <h3 class="lh_24">CHECK-LIST一览：</h3>
-                            <Table :columns="columns" :data="data_1" :border="tableBorder"></Table>
-                        </Row>
-                        <Row class="top_16">
-                            <h3 class="lh_24">关联实体：</h3>
-                            <Table :columns="columns" :data="data_1" :border="tableBorder"></Table>
-                        </Row>
-                        <Row class="top_16">
-                            <h3 class="lh_24">输入单据：</h3>
-                            <Table :columns="columns" :data="inputObjList" :border="tableBorder"></Table>
-                        </Row>
-                        <Row class="top_16">
-                            <h3 class="lh_24">输出单据：</h3>
-                            <Table :columns="columns" :data="outputObjList" :border="tableBorder"></Table>
-                        </Row>
-                    </div>
-                    <div v-else>
-                        <iframe id="is-iframe" src="https://www.baidu.com" frameborder="0" scrolling="no"></iframe>
-                    </div>
+            <Row>
+                <Button type="info" @click=" showFrame = false">文字信息</Button>
+                <Button type="success" @click="showFrame = true">图形信息</Button>
+            </Row>
+            <div class="detailsBox top_16">
+                <div v-if="!showFrame">
+                    <Row>
+                        <ButtonGroup>
+                            <Button>检查项</Button>
+                            <Button>CHECK-LIST</Button>
+                            <Button>输入单据</Button>
+                            <Button>输出单据</Button>
+                            <Button>状态转换</Button>
+                            <Button>单据转换规则</Button>
+                        </ButtonGroup>
+                    </Row>
+                    <Row class="top_16">
+                        <h3 class="lh_24">{{ flowName }} --> 负责人：{{ leaderName }}</h3>
+                        <h3 class="lh_24">目标：</h3>
+                    </Row>
+                    <Row class="top_16">
+                        <h3 class="lh_24">CHECK-LIST一览：</h3>
+                        <Table :columns="columns" :data="data_1" :border="tableBorder"></Table>
+                    </Row>
+                    <Row class="top_16">
+                        <h3 class="lh_24">关联实体：</h3>
+                        <Table :columns="columns" :data="data_1" :border="tableBorder"></Table>
+                    </Row>
+                    <Row class="top_16">
+                        <h3 class="lh_24">输入单据：</h3>
+                        <Table :columns="columns" :data="inputObjList" :border="tableBorder"></Table>
+                    </Row>
+                    <Row class="top_16">
+                        <h3 class="lh_24">输出单据：</h3>
+                        <Table :columns="columns" :data="outputObjList" :border="tableBorder"></Table>
+                    </Row>
                 </div>
+                <div v-else>
+                    <iframe id="is-iframe" :src="iframeUrl" frameborder="0" scrolling="no"></iframe>
+                </div>
+            </div>
             </Col>
         </Row>
     </div>
@@ -55,25 +61,46 @@
 
 <script>
     export default{
-        props: {
-            showChart: {
-                type: Boolean,
-                default: false
-            }
-        },
-        data () {
+        data(){
             return {
                 urls: {
-                    // 获取 checkColumns
-                    getCheckColumns: '/api/workflow/view/getCheckColumns',
                     // 获取 checkData
-                    getCheckData: '/api/workflow/view/getCheckData/'
+                    getCheckData: '/prowf/prowf/repo/findJobMeta4Dis/'
                 },
                 // 默认不显示纵向边框
                 tableBorder: false,
                 // 负责人名字
                 leaderName: '',
-                checkColumns: [],
+                checkColumns: [
+                    {
+                        "type":"index",
+                        "width":60,
+                        "align":"center"
+                    },
+                    {
+                        "title":"名称",
+                        "key":"name",
+                        render: (h, params) => {
+                            return h('div', [
+                                h('a', {
+                                    on: {
+                                        click: () => {
+                                            this.showDetails(params.row)
+                                        }
+                                    }
+                                }, params.row.name)
+                            ])
+                        }
+                    },
+                    {
+                        "title":"负责人",
+                        "key":"dutyName"
+                    },
+                    {
+                        "title":"任务类型",
+                        "key":"jobNodeType"
+                    }
+                ],
                 checkData: [],
                 columns: [
                     {
@@ -91,55 +118,38 @@
                 inputObjList: [],
                 // 输出单据
                 outputObjList: [],
-                // 切换信息
-                showMsg: false
+                //切换信息
+                showFrame:false,
+                iframeUrl:""
             }
         },
         computed: {
-            flowName () {
+            flowName() {
                 return this.$route.params.rowName
             }
         },
         methods: {
-            showDetails (row) {
+            goBack () {
+                this.$router.back(-1)
+            },
+            showDetails(row) {
                 this.leaderName = row['dutyName']
                 let dataAry = ['data_1', 'inputObjList', 'outputObjList']
-                for (let i of dataAry) {
+                for (let i of dataAry){
                     row[i] ? this[i] = row[i] : this[i] = []
                 }
             },
             getRequest (url, argument, key, returnKey) {
                 let reqUrl = `${url}${argument}`
                 this.setUrl(reqUrl).forGet(res => {
-                    if (returnKey === '') {
-                        this[key] = res
-                        if (key === 'checkColumns') {
-                            for (let i of this[key]) {
-                                if (i['key'] === 'name') {
-                                    i['render'] = (h, params) => {
-                                        return h('div', [
-                                            h('a', {
-                                                on: {
-                                                    click: () => {
-                                                        this.showDetails(params.row)
-                                                    }
-                                                }
-                                            }, params.row.name)
-                                        ])
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        this[key] = res[returnKey]
-                    }
+                    returnKey === '' ? this[key] = res : this[key] = res[returnKey]
                 })
             }
         },
-        mounted () {
-            this.getRequest(this.urls.getCheckColumns, '', 'checkColumns', '')
+        mounted(){
             this.getRequest(this.urls.getCheckData, this.$route.params.rowId, 'checkData', '')
-            this.showMsg = this.showChart
+            //在画图需要的id
+            this.iframeUrl=`/static/drawFormStatusChart.html?id=${this.$route.params.rowId}`
         }
     }
 </script>
@@ -165,10 +175,11 @@
         position: relative;
     }
     #is-iframe{
-        border: 1px solid #000 ;
-        width:800px;
-        height:600px;
-        position: absolute;
-        right: 0;
+        /*border: 1px solid #000 ;*/
+        width:100%;
+        min-height:600px;
+    }
+    .marBot {
+        margin-bottom: 14px;
     }
 </style>
