048760d0b87786863e46d03c25a7b96f97e52d5d
Fangzy <1003455913@qq.com>
2017-12-04 09:23:03 +0800

    2个新组件

---
+++ src/components/base/m-button-show.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <Button :type="define.type || 'default'" :size="define.size || 'default'" @click="handleClick">{{define.text}}</Button>
+        <mLayer :value="showLayer" :titleText="define.text">
+            <iframe
+                :src="iframeUrl"
+                frameborder="0"
+                scrolling="no"
+                style="width: 100%; min-height:600px;"
+            ></iframe>
+        </mLayer>
+    </div>
+</template>
+<script>
+    import {urlToString} from 'utils/actionUtils'
+
+    export default {
+        props:{
+            define:{}
+        },
+        data () {
+            return {
+                showLayer:false,
+                iframeUrl:`/static/drawFormStatusChart.html?id=1872436001224704`,
+            }
+        },
+        mounted () {
+            this.iframeUrl = urlToString(this.define.url)
+        },
+        methods: {
+            handleClick(){
+                this.showLayer =true
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/base/m-span.vue
@@ -0,0 +1,17 @@
+<template>
+    <span :style="{ fontSize:define.size || '14px' ,
+                    color:define.color || '#333',
+                    padding:define.padding || '0',
+                    margin:define.margin || '0'}">{{define.text}}</span>
+</template>
+<script>
+    export default {
+        props: ['define'],
+        computed: {
+        },
+        methods: {
+        }
+    }
+</script>
+<style scoped>
+</style>
