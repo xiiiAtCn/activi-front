7f66d83b48b0a4de0cf92dd1501d29059199f0e7
Fangzy <1003455913@qq.com>
2017-12-05 10:14:55 +0800

    m-row 空数据

--- src/components/base/m-row.vue
+++ src/components/base/m-row.vue
@@ -1,19 +1,21 @@
 <template>
     <Row :gutter="gutter" :type="type">
         <transition-group name="fade" mode="out-in">
-            <Col 
-                v-for="item in content" 
-                :span="item.ui_define.col" 
-                :push="item.ui_define.offset" 
+            <Col
+                v-for="item in content"
+                :span="item.ui_define.col"
+                :push="item.ui_define.offset"
                 :key="item.ui_id"
             >
-                <component 
+                <component
+                    v-if="item.ui_content ? true : false"
                     :is="item.ui_content[0].ui_type"
                     :uid="item.ui_content[0].ui_id"
                     :define="item.ui_content[0].ui_define"
                     :content="item.ui_content[0].ui_content"
                 >
                 </component>
+                <div v-else class="box"></div>
             </Col>
         </transition-group>
     </Row>
@@ -32,3 +34,8 @@
       }
   }
 </script>
+<style scoped>
+    .box{
+        padding: 10px;
+    }
+</style>
