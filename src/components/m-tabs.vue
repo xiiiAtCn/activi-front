2d96d14ead34be3ae877bbc1a9d7dd12f37dfd8b
Fangzy <1003455913@qq.com>
2017-12-08 16:12:36 +0800

    tabs url

---
+++ src/components/m-tabs.vue
@@ -0,0 +1,59 @@
+<template>
+    <div :class="define.mode === 'horizontal' ? 'horizontal' : 'vertical'">
+        <Menu :mode="define.mode" :active-name="activeNumber" @on-select="handleSelect" class="menu">
+            <MenuItem v-for="(item, key) in content" :key="key" :name="key">
+                <Icon v-if="item.icon" type="item.icon"></Icon>
+                    {{item.ui_define.title}}
+            </MenuItem>
+        </Menu>
+        <div class="container">
+            <div v-for="(item, key) in content" :key="key">
+                <component
+                    v-show="menuName === key"
+                    :is="item.ui_content[0].ui_type"
+                    :define="item.ui_content[0].ui_define"
+                    :content="item.ui_content[0].ui_content"
+                    :form="item.ui_content[0].ui_form"
+                ></component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['define','content'],
+        data () {
+            return {
+                theme1: 'light',
+                menuName: 0,
+                activeNumber:0
+            }
+        },
+        mounted(){
+        },
+        methods:{
+            handleSelect(name){
+                this.menuName = Number(name)
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    div.horizontal .menu{
+        margin-bottom: 20px;
+    }
+    div.vertical:after {
+        display: block;
+        clear: both;
+        content:"";
+    }
+    div.vertical .menu{
+        float: left;
+    }
+    div.vertical .container{
+        padding-left: 15px;
+        height: 100%;
+    }
+</style>
