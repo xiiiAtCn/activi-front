7f941d6ab9b1b9e140b61faa7c67c18f4c7939ea
Fangzy <1003455913@qq.com>
2017-12-19 16:34:33 +0800

    m-boolean

--- src/components/base/m-boolean.vue
+++ src/components/base/m-boolean.vue
@@ -15,6 +15,11 @@
 
     export default {
         mixins: [mixin],
+        data(){
+          return {
+              isError:false
+          }
+        },
         methods: {
             valid(){
                 this.$store.dispatch(ELEMENT_VALIDATE_RESULT, {[this.name]: false, form: this.form})
