7edf450972ecee86184b1dd2e3082cbcbb93d856
Fangzy <1003455913@qq.com>
2017-12-19 16:31:18 +0800

    表格单选框

--- src/components/base/m-boolean.vue
+++ src/components/base/m-boolean.vue
@@ -15,25 +15,9 @@
 
     export default {
         mixins: [mixin],
-        data(){
-          return {
-              isError:true
-          }
-        },
         methods: {
-            change(){
-                if(this.readonly){return}
-                this.isError = false
-            },
             valid(){
-                if (!this.readonly) {
-                    if (this.required) {
-                        if(this.isError) { this.errorMessage = '请选择必填项'}
-                    }
-                    this.$store.dispatch(ELEMENT_VALIDATE_RESULT, {[this.name]: this.isError, form: this.form})
-                } else {
-                    this.$store.dispatch(ELEMENT_VALIDATE_RESULT, {[this.name]: false, form: this.form})
-                }
+                this.$store.dispatch(ELEMENT_VALIDATE_RESULT, {[this.name]: false, form: this.form})
             }
         }
     }
