62798135b0b6d33f474b15a65fc494f00787c671
xiii <xiii.at.cn@gmail.com>
2017-12-22 09:41:36 +0800

    fix some bugs when edit table data

---
+++ src/test/components/selectOneTest.vue
@@ -0,0 +1,36 @@
+<template>
+    <Button @click="click">点击</Button>
+</template>
+<script>
+    import { dispatch } from 'utils/actionUtils'
+
+    let action = {
+        type: 'selectOne',
+        message: '请选择一项',
+        buttons: [
+            {
+                text: '审批通过',
+                size: 'large',
+                type: 'primary',
+                action: {
+                    type: 'asLink'
+                }
+            }, 
+            {
+                text: '审批拒绝',
+                size: 'large',
+                type: 'primary',
+                action: {
+                    type: 'asLink'
+                }
+            }
+        ]
+    }
+    export default {
+        methods: {
+            click() {
+                dispatch(action)
+            }
+        }
+    }
+</script>
