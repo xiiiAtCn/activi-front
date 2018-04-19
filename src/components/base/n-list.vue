763282e677cd63bf7e1ee42db1c708da02c3701d
lungern <xiii.at.cn@gmail.com>
2018-04-19 10:58:07 +0800

    progress: render businessKey which can jump to next page

--- src/components/base/n-list.vue
+++ src/components/base/n-list.vue
@@ -10,7 +10,9 @@
     </div>
 </template>
 <script>
-    import { getData } from 'utils/actionUtils'
+    import { getData, dispatch } from 'utils/actionUtils'
+    import { replace, addQuery } from 'utils/request-addon'
+    import _ from 'lodash'
     export default {
         name: 'n-list',
         props: {
@@ -25,14 +27,57 @@
             }
         },
         mounted() {
-            this.columns = this.define.cols
+            debugger
+            let { cols , businessKey, detailUrl } = this.define
+            this.columns = this.handleColumns(cols, businessKey, detailUrl)
             let { dataUrl } = this.define
             getData(dataUrl, (data) => {
                 this.data = data
             })
         },
         methods: {
-
+            handleColumns(metaList, businessKey, detailUrl) {
+                let columns = []
+                metaList.forEach((meta) => {
+                    let column = {}
+                    column.title = meta.caption
+                    column.key = meta.key
+                    let { type, key } = meta
+                    if( key === businessKey) {
+                        column.render = ( h, { row }) => {
+                            return h('a', {
+                                on: {
+                                    click() {
+                                        let url = replace(detailUrl.url, detailUrl.pathParams)
+                                        url = addQuery(url, {
+                                            id: row['id']
+                                        })
+                                        let bak = {
+                                            url,
+                                            type: 'link'
+                                        } 
+                                        dispatch(bak)
+                                    }
+                                },
+                            }, row[key]) 
+                        }   
+                    }
+                    switch(type) {
+                    case 'string':
+                        break
+                    case 'select':
+                        column.render = (h, {row}) => {
+                            let value = row[meta.key]
+                            return h('span', meta.options[value])
+                        }
+                        break
+                    default:
+                        break
+                    }
+                    columns.push(column)
+                })
+                return columns
+            }
         }
     }
 </script>
