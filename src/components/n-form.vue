9960300670fb7b4098d6aa2e329c323f7cfb2242
lungern <xiii.at.cn@gmail.com>
2018-05-16 18:17:06 +0800

    progress: split form from n-main

---
+++ src/components/n-form.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            define: {
+                required: true
+            },
+            content: {
+                required: true
+            }
+        },
+        computed:{
+
+        },
+        watch: {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+            
+        }
+    }
+</script>
+<style>
+
+</style>
